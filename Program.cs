defdf992df137765d34812e4199a104f66f1a6ff
Vadim <01prosto01@mail.ru>
2023-05-31 22:06:13 +1000

    1 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int GetNumber (string info)
+{
+    Console.WriteLine (info);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+//Task1________________________________________
+int numberA = GetNumber ("Введите первое целое число");
+int numberB = GetNumber ("Введите второе целое число");
+int result = numberA;
+
+for (int i = 1; i < numberB; i++)
+{
+    result = result * numberA;
+}
+
+Console.WriteLine (result);
